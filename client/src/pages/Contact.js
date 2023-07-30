1a39d157e407339ec207264d52312c79f4fb3692
Nitish Kumar Jha <nitishjha913@gmail.com>
2023-07-30 21:20:59 +0530

    UI update

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,36 +1,152 @@
+            
 import React from "react";
 import Layout from "./../components/Layout/Layout";
 import { BiMailSend, BiPhoneCall, BiSupport } from "react-icons/bi";
+import { styled } from "styled-components";
+
 const Contact = () => {
   return (
     <Layout title={"Contact us"}>
-      <div className="row contactus ">
-        <div className="col-md-6 ">
-          <img
-            src="/images/contactus.jpeg"
-            alt="contactus"
-            style={{ width: "100%" }}
-          />
+    <ContactForm>
+    <div className="contact3 py-5">
+  <div className="row no-gutters">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-6">
+          <div className="card-shadow">
+            <img alt="contact image" src="/images/contactus.jpeg" className="img-fluid" />
+          </div>
         </div>
-        <div className="col-md-4">
-          <h1 className="bg-dark p-2 text-white text-center">CONTACT US</h1>
-          <p className="text-justify mt-2">
-            any query and info about prodduct feel free to call anytime we 24X7
-            vaialible
-          </p>
-          <p className="mt-3">
-            <BiMailSend /> : www.help@ecommerceapp.com
-          </p>
-          <p className="mt-3">
-            <BiPhoneCall /> : 012-3456789
-          </p>
-          <p className="mt-3">
-            <BiSupport /> : 1800-0000-0000 (toll free)
-          </p>
+        <div className="col-lg-6">
+          <div className="contact-box ml-3">
+            <h1 className="font-weight-light mt-2">Quick Contact</h1>
+            <form className="mt-4" action="https://formspree.io/f/xgebgpeg" method='POST'>
+              <div className="row">
+                <div className="col-lg-12">
+                  <div className="form-group mt-2">
+                    <input className="form-control" name="name" type="text" placeholder="name" />
+                  </div>
+                </div>
+                <div className="col-lg-12">
+                  <div className="form-group mt-2">
+                    <input className="form-control" name="email" type="email" placeholder="email address" />
+                  </div>
+                </div>
+                <div className="col-lg-12">
+                  <div className="form-group mt-2">
+                    <textarea className="form-control" name="message" rows="3" placeholder="message"></textarea>
+                  </div>
+                </div>
+                <div className="col-lg-12">
+                  <button type="submit" value="send" className="btn btn-danger-gradiant mt-3 text-white border-0 px-3 py-2"><span> SUBMIT</span></button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="col-lg-12">
+          <div className="card mt-4 border-0 mb-4">
+            <div className="row">
+              <div className="col-lg-4 col-md-4">
+                <div className="card-body d-flex align-items-center c-detail pl-0">
+                  <div className="mr-3 align-self-center">
+                    <img alt="contact image" src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/contact/icon1.png" />
+                  </div>
+                  <div className="contact-items">
+                    <h6 className="font-weight-medium">Address</h6>
+                    <p className="">New Delhi.
+                      <br /> India</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-4 col-md-4">
+                <div className="card-body d-flex align-items-center c-detail">
+                  <div className="mr-3 align-self-center">
+                    <img alt="contact image" src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/contact/icon2.png" />
+                  </div>
+                  <div className="contact-items">
+                    <h6 className="font-weight-medium">Phone</h6>
+                    <p className="">8888888888
+                      <br /> 630 446 8851</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-4 col-md-4">
+                <div className="card-body d-flex align-items-center c-detail">
+                  <div className="mr-3 align-self-center">
+                    <img alt="contact image"  src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/contact/icon3.png" />
+                  </div>
+                  <div className="contact-items">
+                    <h6 className="font-weight-medium">Email</h6>
+                    <p className="">
+                      xyz@gmail.com
+                      <br /> 123@gmail.com
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+    </div>
+  </div>
+</div>
+</ContactForm>
     </Layout>
   );
 };
 
 export default Contact;
+
+
+
+const ContactForm=styled.div`
+margin-top:60px;
+padding:0 20px ;
+
+.contact-items{
+  margin-left:10px;
+}
+.contact3 {
+  font-family: "Montserrat", sans-serif;
+  color: #8d97ad;
+  font-weight: 300;
+}
+
+.contact3 h1,
+.contact3 h2,
+.contact3 h3,
+.contact3 h4,
+.contact3 h5,
+.contact3 h6 {
+  color: #3e4555;
+}
+
+.contact3 .font-weight-medium {
+  font-weight: 500;
+}
+
+.contact3 .card-shadow {
+  -webkit-box-shadow: 0px 0px 30px rgba(115, 128, 157, 0.1);
+  box-shadow: 0px 0px 30px rgba(115, 128, 157, 0.1);
+}
+
+.contact3 .btn-danger-gradiant {
+  background: #ff4d7e;
+  background: -webkit-linear-gradient(legacy-direction(to right), #ff4d7e 0%, #ff6a5b 100%);
+  background: -webkit-gradient(linear, left top, right top, from(#ff4d7e), to(#ff6a5b));
+  background: -webkit-linear-gradient(left, #ff4d7e 0%, #ff6a5b 100%);
+  background: -o-linear-gradient(left, #ff4d7e 0%, #ff6a5b 100%);
+  background: linear-gradient(to right, #ff4d7e 0%, #ff6a5b 100%);
+}
+
+.contact3 .btn-danger-gradiant:hover {
+  background: #ff6a5b;
+  background: -webkit-linear-gradient(legacy-direction(to right), #ff6a5b 0%, #ff4d7e 100%);
+  background: -webkit-gradient(linear, left top, right top, from(#ff6a5b), to(#ff4d7e));
+  background: -webkit-linear-gradient(left, #ff6a5b 0%, #ff4d7e 100%);
+  background: -o-linear-gradient(left, #ff6a5b 0%, #ff4d7e 100%);
+  background: linear-gradient(to right, #ff6a5b 0%, #ff4d7e 100%);
+}
+`
